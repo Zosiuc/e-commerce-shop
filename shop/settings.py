711f3974de2e10192368cbbdc4bb6bd1c820bf0c
yazanalhayek <yazzhayek1998@gmail.com>
2025-07-11 17:45:52 +0200

    static dir

--- shop/settings.py
+++ shop/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 # Statische bestanden
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Media bestanden (optioneel)
 MEDIA_URL = "/media/"
