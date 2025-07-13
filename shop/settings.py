43cd647b58733d3c3ea02b43663bf1a0c6ee3d1a
yazanalhayek <yazzhayek1998@gmail.com>
2025-07-13 23:07:16 +0200

    icon

--- shop/settings.py
+++ shop/settings.py
@@ -94,6 +94,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Media bestanden (optioneel)
 MEDIA_URL = "/media/"
