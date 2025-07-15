6a0f9821d21997860fcb3f77a1901b18b8df6c83
yazanalhayek <yazzhayek1998@gmail.com>
2025-07-16 01:40:32 +0200

    nieuwe header-mobiel css

--- static/shop/js/script.js
+++ static/shop/js/script.js
@@ -12,15 +12,45 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
+// Show sidebar
 function toggleSide_bar() {
-    const side_bar = document.getElementById('side_bar');
 
-    if (side_bar.style.display === 'none') {
-        side_bar.style.display = 'flex';
-    } else {
+    const side_bar = document.getElementById('side_bar');
+    console.log(side_bar.style.display);
+
+    if (side_bar.style.display === 'flex') {
         side_bar.style.display = 'none';
+    } else {
+        side_bar.style.display = 'flex';
     }
+    console.log(side_bar.style.display);
+}
+
+// show search field
+function toggleSearch_bar() {
+    const search_bar = document.getElementById('search_bar');
+    const search_button = document.getElementById('search_button');
+    const resultaten = document.getElementById("resultaten");
+    console.log(search_bar.style.display);
+    if (search_bar.style.display === 'flex') {
+        search_bar.style.display = 'none';
+        search_button.style.width = '40px';
+        resultaten.style.display = 'none';
+    }else {
+        search_bar.style.display = 'flex';
+        search_button.style.width = '220px';
+        search_button.style.borderRadius = '7px';
+        search_button.style.padding = '0 10px';
+        search_bar.focus();
+    }
+    console.log(search_bar.style.display);
+    // Verberg search veld als gebruiker ergens anders klikt
+    /*document.addEventListener("click", (e) => {
+        if (!search_bar.contains(e.target) ) {
+            search_bar.style.display = 'none';
+        }
+    });*/
+    //console.log(search_bar.style.display);
 }
 
 
@@ -28,12 +58,12 @@ function toggleSide_bar() {
 async function zoek() {
     const zoekveld = document.getElementById("zoekveld");
     const resultaten = document.getElementById("resultaten");
-
+    resultaten.style.display = 'flex';
     const query = zoekveld.value.trim();
 
     if (query === "") {
         resultaten.innerHTML = "";
-        resultaten.classList.add("hidden");
+        resultaten.style.display = 'none';
         return;
     }
 
@@ -45,6 +75,7 @@ async function zoek() {
 
         if (data.length === 0) {
             const li = document.createElement("li");
+            li.classList.remove("list_item");
             li.textContent = "Geen resultaten";
             resultaten.appendChild(li);
         } else {
